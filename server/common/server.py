528fd94809f3593ebad6d83f96bc593719708e20
ext-frfang <ext_frfang@mercadolibre.com>
2024-08-31 00:30:35 -0300

    add logic to server so that it gracefuly shuts down

--- server/common/server.py
+++ server/common/server.py
@@ -1,5 +1,6 @@
 import socket
 import logging
+import signal
 
 
 class Server:
@@ -8,6 +9,8 @@ class Server:
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
+        self._run = True
+        signal.signal(signal.SIGTERM, self.__handle_sigterm)
 
     def run(self):
         """
@@ -20,9 +23,14 @@ class Server:
 
         # TODO: Modify this program to handle signal to graceful shutdown
         # the server
-        while True:
-            client_sock = self.__accept_new_connection()
-            self.__handle_client_connection(client_sock)
+        while self._run:
+            try:
+                client_sock = self.__accept_new_connection()
+                self.__handle_client_connection(client_sock)
+            except OSError as _:
+                if not self._run:
+                    break
+                raise
 
     def __handle_client_connection(self, client_sock):
         """
@@ -56,3 +64,8 @@ class Server:
         c, addr = self._server_socket.accept()
         logging.info(f'action: accept_connections | result: success | ip: {addr[0]}')
         return c
+
+    def __handle_sigterm(self, signum, frame):
+        # TODO : add compliant logging
+        self._server_socket.close()
+        self._run = False
