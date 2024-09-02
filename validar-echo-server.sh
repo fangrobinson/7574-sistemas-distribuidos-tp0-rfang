d7d65f3c359ecdda9624ad925de03fe93717393a
ext-frfang <ext_frfang@mercadolibre.com>
2024-09-02 19:23:48 -0300

    rm sudo

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 TEST_MESSAGE=$(openssl rand -base64 12)
 
-RESPONSE=$(sudo docker run --rm --network tp0_testing_net busybox sh -c "echo $TEST_MESSAGE | nc server 12345")
+RESPONSE=$(docker run --rm --network tp0_testing_net busybox sh -c "echo $TEST_MESSAGE | nc server 12345")
 
 if [ "$RESPONSE" == "$TEST_MESSAGE" ]; then
     echo "action: test_echo_server | result: success"
