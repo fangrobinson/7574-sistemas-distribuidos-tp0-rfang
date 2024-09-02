b33f9ed8b7ad0303f68a805bc23b3d6ceaf825ed
ext-frfang <ext_frfang@mercadolibre.com>
2024-09-02 19:30:12 -0300

    fix comparison

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -3,7 +3,7 @@ TEST_MESSAGE=$(openssl rand -base64 12)
 
 RESPONSE=$(docker run --rm --network tp0_testing_net busybox sh -c "echo $TEST_MESSAGE | nc server 12345")
 
-if [ "$RESPONSE" == "$TEST_MESSAGE" ]; then
+if [ "$RESPONSE" = "$TEST_MESSAGE" ]; then
     echo "action: test_echo_server | result: success"
 else
     echo "action: test_echo_server | result: fail"
